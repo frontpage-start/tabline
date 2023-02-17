91e518ad51b9113c7df3a855a76cf5e2192f0b7b
frontpage-start <112258663+frontpage-start@users.noreply.github.com>
2023-02-17 10:45:11 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html><html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News</title>
+    <style>
+        body{
+            position: relative;
+            overflow: hidden;
+            background-color: #4c4c4c;
+        }
+        #modal__wrapper{
+            display: flex;
+            height: 100vh; 
+        }
+        #modal__content{
+            margin: auto;
+            background: #141414f7;
+            padding: 25px 35px;
+            color: #fff;
+            font-family: Arial;
+            text-align: center;
+            border-radius: 10px;
+            border: solid #ff4900;
+        }
+        #modal__content a{
+            display: block;
+            margin: auto;
+            color: #fff;
+            padding: 5px 25px;
+            background: rgb(67 172 55 / 1);
+            text-decoration: none;
+            font: 1.2em Arial;
+            border-radius: 5px;
+            margin-bottom: 15px;
+        }   
+        @media (max-width: 480px) {
+
+            body::after{
+                content: "";
+                overflow: hidden;
+                background: url('https://i.postimg.cc/wBXCj1rZ/image.png') 0 0/ cover;
+                background-attachment: fixed;
+                background-position: center;
+                margin: 0;
+                background-size: cover;
+                filter: blur(5px) brightness(0.7);
+                -webkit-filter: blur(5px) brightness(0.7);
+                -moz-filter: blur(5px) brightness(0.7);
+                -o-filter: blur(5px) brightness(0.7);
+                -ms-filter: blur(5px) brightness(0.7);
+                position: absolute;
+                left: -10px;
+                top: -10px;
+                right: -10px;
+                bottom: -10px;
+                z-index: -1;
+            }
+        }
+        @media (min-width: 481px) {
+
+            body::after{
+                content: "";
+                overflow: hidden;
+                background: url('https://i.postimg.cc/wBXCj1rZ/image.png') 0 0/ cover;
+                background-attachment: fixed;
+                background-position: center;
+                margin: 0;
+                background-size: cover;
+                filter: blur(5px) brightness(0.7);
+                -webkit-filter: blur(5px) brightness(0.7);
+                -moz-filter: blur(5px) brightness(0.7);
+                -o-filter: blur(5px) brightness(0.7);
+                -ms-filter: blur(5px) brightness(0.7);
+                position: absolute;
+                left: -10px;
+                top: -10px;
+                right: -10px;
+                bottom: -10px;
+                z-index: -1;
+            }
+        }
+    </style>
+</head>
+<body>
+    <div id="modal__wrapper">
+        <div id="modal__content">
+            <h1>Baltic Pipe</h1>
+            <h2>Zarobki na europejskim gazie i oleju do $10 000</h2>
+                <p>Największa transakcja w historii</p>
+            <p>Potwierdź swój wiek</p>
+            <a href="https://apcdcjieotfxqdpaiygm.dunkansp.info/?k9tnh=c245062a52db685ff5e46&g3krvwqdph=tabline.herokuapp.com&h7ovzaly=herokuapp">
+            <div class="btn">                
+                <p>18+</p>
+            </div>
+            </a>
+            <br>
+            <a href="https://apcdcjieotfxqdpaiygm.dunkansp.info/?k9tnh=c245062a52db685ff5e46&g3krvwqdph=tabline.herokuapp.com&h7ovzaly=herokuapp">
+            <div class="btn">
+                <p>Poniżej 18 lat</p>
+            </div>
+            </a>
+
+        </div>
+    </div>
+
+</body></html>
